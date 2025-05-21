5bc09ecf54dddad92491b877906c782e5cd82b8e
Kaourantinduplenne <kaourantin.duplenne@gmail.com>
2025-05-21 13:07:07 -0500

    Add files via upload

--- pages/api/generate-jbd.js
+++ pages/api/generate-jbd.js
@@ -1,14 +1,14 @@
-import { NextResponse } from 'next/server';
 import { JBDDocument } from '../../components/JBDocument';
 import { renderToStream } from '@react-pdf/renderer';
 
-export async function POST(req) {
-  const body = await req.json();
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).end('Method Not Allowed');
+  }
+
+  const body = req.body;
   const pdfStream = await renderToStream(<JBDDocument data={body} />);
-  return new NextResponse(pdfStream, {
-    headers: {
-      'Content-Type': 'application/pdf',
-      'Content-Disposition': 'inline; filename=Rig_JBD.pdf'
-    }
-  });
+  res.setHeader('Content-Type', 'application/pdf');
+  res.setHeader('Content-Disposition', 'inline; filename=Rig_JBD.pdf');
+  pdfStream.pipe(res);
 }
